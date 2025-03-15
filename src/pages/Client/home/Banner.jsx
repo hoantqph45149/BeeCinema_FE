d5afc0cea7f113084a3c8c6150f61f4732b92f98
tranquochoan <hoantqph45149@fpt.edu.vn>
2025-03-15 16:12:23 +0700

    update camelcase name file

---
+++ src/pages/Client/home/Banner.jsx
@@ -0,0 +1,84 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useState, useEffect } from "react";
+import { useFetch } from "../../../Hooks/useCRUD";
+
+const Banner = () => {
+  const { data } = useFetch(["bannerActive"], "/banners/active");
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+    if (data?.banner) {
+      setBanners(data.banner.img_thumbnail_url);
+    }
+  }, [data]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % banners.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + banners.length) % banners.length);
+  };
+
+  // Chạy auto slide mà không làm ảnh hưởng đến API
+  useEffect(() => {
+    if (banners.length > 1) {
+      const interval = setInterval(nextSlide, 5000);
+      return () => clearInterval(interval);
+    }
+  }, [currentSlide, banners.length]);
+
+  return (
+    <div className="w-full pb-5">
+      <div className="relative w-full cm:h-[528px] md:h-[342px] lg:h-[500px] xl:h-[800px] flex overflow-hidden">
+        {/* Slides */}
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {banners?.map((banner, index) => (
+            <div key={index} className="min-w-full h-full">
+              <img
+                src={banner}
+                alt={`Slide-${index}`}
+                className="w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Navigation buttons */}
+        {banners.length > 1 && (
+          <>
+            <div
+              onClick={prevSlide}
+              className="w-10 h-10 cm:hidden shadow-md rounded-full absolute top-1/2 transform -translate-y-1/2 cursor-pointer z-1000 bg-white left-3 flex justify-center items-center"
+            >
+              <ChevronLeft size={48} strokeWidth={5} />
+            </div>
+            <div className="absolute bottom-3 flex justify-center items-center gap-3 w-full">
+              {banners.map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => setCurrentSlide(index)}
+                  className={`bg-gray-300 cursor-pointer rounded-full transition-all duration-400 ease-in-out ${
+                    index === currentSlide ? "w-5 h-5" : "w-3 h-3"
+                  }`}
+                ></span>
+              ))}
+            </div>
+            <div
+              onClick={nextSlide}
+              className="w-10 h-10 cm:hidden shadow-md rounded-full absolute top-1/2 transform -translate-y-1/2 cursor-pointer z-1000 bg-white right-3 flex justify-center items-center"
+            >
+              <ChevronRight size={48} strokeWidth={5} />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Client/home/index.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "../../../Components/Common/MovieCard";
+import TabMovies from "../../../Components/Common/TabMovies";
+import Banner from "./Banner";
+import { useAuthContext } from "../../../Contexts/auth/UseAuth";
+
+import VerifiedEmail from "../../Auth/verified-email";
+import { useFetch } from "../../../Hooks/useCRUD";
+import { useBrancheContext } from "../../../Contexts/branche/useBrancheContext";
+import api from "../../../apis/axios";
+import { Modal } from "reactstrap";
+
+const Home = () => {
+  const { cinema } = useBrancheContext();
+  const { authUser } = useAuthContext();
+
+  const [phimDangChieu, setPhimDangChieu] = useState([]);
+  const [phimSapChieu, setPhimSapChieu] = useState([]);
+  const [xuatChieuDB, setXuatChieuDB] = useState([]);
+  const [openModalVeryfiedEmail, setOpenModalVeryfiedEmail] = useState(false);
+
+  useEffect(() => {
+    if (cinema?.id) {
+      const fetchData = async () => {
+        try {
+          const { data } = await api.get(`/movies/tab?cinema_id=${cinema.id}`);
+
+          if (!data) {
+            return;
+          }
+
+          setPhimDangChieu(
+            data.moviesShowing
+              ? [...data.moviesShowing].sort(
+                  (a, b) => b.showtimes_count - a.showtimes_count
+                )
+              : []
+          );
+          setPhimSapChieu(
+            data.moviesUpcoming
+              ? [...data.moviesUpcoming].sort(
+                  (a, b) => b.showtimes_count - a.showtimes_count
+                )
+              : []
+          );
+          setXuatChieuDB(
+            data.moviesSpecial
+              ? [...data.moviesSpecial].sort(
+                  (a, b) => b.showtimes_count - a.showtimes_count
+                )
+              : []
+          );
+        } catch (error) {
+          console.error("Lỗi khi fetch dữ liệu:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [cinema]);
+
+  useEffect(() => {
+    if (authUser?.user && authUser?.user?.email_verified_at === null) {
+      setOpenModalVeryfiedEmail(true);
+    }
+  }, [authUser?.user]);
+
+  const movieTabs = [
+    {
+      label: "PHIM SẮP CHIẾU",
+      content: (
+        <div className="container">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-12">
+            {phimSapChieu?.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        </div>
+      ),
+    },
+    {
+      label: "PHIM ĐANG CHIẾU",
+      content: (
+        <div className="container">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-12">
+            {phimDangChieu?.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        </div>
+      ),
+    },
+    {
+      label: "SUẤT CHIẾU ĐẶC BIỆT",
+      content: (
+        <div className="container">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-12">
+            {xuatChieuDB?.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <>
+      {openModalVeryfiedEmail && (
+        <Modal
+          isOpen={true}
+          onClose={() => setOpenModalVeryfiedEmail(false)}
+          title="Xác thực Email"
+          isFooter={false}
+        >
+          <VerifiedEmail />
+        </Modal>
+      )}
+      <div className="hidden md:block">
+        <Banner />
+      </div>
+
+      <div className="my-10">
+        <TabMovies
+          tabs={movieTabs}
+          defaultTab="PHIM ĐANG CHIẾU"
+          onTabChange={(tab) => {
+            // console.log("Tab changed!", tab);
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Home;
