188d8f3eea891770bde5d09d998c24c8b18d1f3f
tranquochoan <hoantqph45149@fpt.edu.vn>
2025-03-15 19:02:26 +0700

    fix vite config and remove  vercel json

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor"; // Nhóm React thành 1 file riêng
-            if (id.includes("lodash")) return "lodash"; // Nhóm lodash riêng
-            return "vendor"; // Nhóm các thư viện khác
-          }
-        },
-      },
-    },
-    minify: "terser", // Dùng terser để nén file
-    terserOptions: {
-      compress: {
-        drop_console: true, // Xóa console.log khi build
-        drop_debugger: true,
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Tăng giới hạn cảnh báo chunk lên 1000kB
-  },
 });
