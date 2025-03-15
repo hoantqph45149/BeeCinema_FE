133e214d957956e9510839615100cc75e57ceb75
tranquochoan <hoantqph45149@fpt.edu.vn>
2025-03-15 18:43:40 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Dùng đường dẫn tương đối khi deploy lên Vercel
+  base: "/", // Sửa base thành "/"
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
-    outDir: "dist", // Đảm bảo thư mục build đúng
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("lodash")) return "lodash";
-            return "vendor";
-          }
-        },
-      },
-    },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    chunkSizeWarningLimit: 1000,
+    outDir: "dist",
   },
 });
